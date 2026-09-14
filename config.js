8408a52aabfe22759a427b936d2dbe87b30c3d5c
Shanmugaraj T <137999395+shanmugaraj680@users.noreply.github.com>
2026-09-14 19:08:02 +0530

    Change Supabase publishable key
    Updated Supabase publishable key in config.js.

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Add the Supabase publishable key from your Supabase project here.
 // Do NOT use a service_role/secret key in this file.
-window.NFM_SUPABASE_KEY = 'PASTE_YOUR_SUPABASE_PUBLISHABLE_KEY_HERE';
+window.NFM_SUPABASE_KEY = 'sb_publishable_T-_uZHkFL-pWWyFuOyow9w_3XXTG5NK';
